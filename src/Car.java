7bb4eaf92358ac2c71bc9a295c1c495adb3c3aaa
Lennert Ádám <lennertadam@ktch.hu>
2025-02-25 11:02:09 +0100

    Filereader javítva

--- src/Car.java
+++ src/Car.java
@@ -8,5 +8,14 @@ public class Car {
     Double price;
     Boolean clime;
     LocalDate validity;
+    public Car(String plate, String brand, Integer year, String fuel, Double price, Boolean clime, LocalDate validity) {
+        this.plate = plate;
+        this.brand = brand;
+        this.year = year;
+        this.fuel = fuel;
+        this.price = price;
+        this.clime = clime;
+        this.validity = validity;
+    }
     
 }
--- src/Fileread.java
+++ src/Fileread.java
@@ -1,12 +1,15 @@
 import java.io.FileReader;
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 
 public class Fileread {
-    public static ArrayList<String> get_benzin() throws Exception{
-        ArrayList<String>cars=new ArrayList<String>();
-        FileReader filereader=new FileReader("jarib.txt");
-        filereader.close();
-        return cars;
+    public static ArrayList<Car> get_benzin() throws Exception{
+        ArrayList<Car>car_list=new ArrayList<Car>();
+        // FileReader filereader=new FileReader("jarib.txt");
+        // String temp_car=filereader.read()
+        car_list.add(new Car("AAA-111", "Honda", 2000, "Benzin", 1200.0, true, LocalDate.parse("2026-04-04")));
+        // filereader.close();
+        return car_list;
     }
 }
